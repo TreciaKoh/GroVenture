6201fa8ea1afb188fe0eb3ada89d4e6a493b68c0
mmphang <mmphang.2012@sis.smu.edu.sg>
2014-06-09 11:14:34 +0800

    Added root 'pages#loginPage'

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
   get 'pages/deleteRecord'
   
   get 'pages/loginRecord'
+  
+  root 'pages#loginPage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
